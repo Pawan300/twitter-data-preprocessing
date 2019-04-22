531b46fadc5a87db8557f56b4c547465d1323ac9
Pawan bisht <48342428+Pawan300@users.noreply.github.com>
2019-04-22 11:23:54 +0530

    Add files via upload

---
+++ extracting tweets.R
@@ -0,0 +1,23 @@
+library('twitteR')
+library("ROAuth")
+accessToken="1088035550978691072-2kR3vIaNc0ojwx4pGU5ugIUzSuC1RI"
+accessTokenSecret="CmgSgt1CKfgEeIOSiAsPwC05cVGU3dUwkpnp2hm0Lllca"
+consumerKey="DGjnvrSC3zY3CNhYJVazdf132"
+consumerSecret="q65jCQvsl1xX8lKSPsnJKAKg5viS7OM3t8gxV1fabLtGx0IkPI"
+setup_twitter_oauth (consumerKey, consumerSecret, accessToken, accessTokenSecret) 
+tweets <- searchTwitter("tcs",n=10000,lang='en',since = "2019-04-01" ,until = "2019-04-10")
+tweetsDF <- twListToDF(tweets)
+write.csv(tweetsDF,"C:\\Users\\pawan_300\\Desktop\\tweets.csv")
+
+
+library('twitteR')
+library("ROAuth")
+accessToken="*******************"
+accessTokenSecret="******************"
+consumerKey="*******************"
+consumerSecret="*********************"
+setup_twitter_oauth (consumerKey, consumerSecret, accessToken, accessTokenSecret) 
+tweets <- searchTwitter("Search Query",n=10000,lang='en',since = "2019-04-01" ,until = "2019-04-10")
+tweetsDF <- twListToDF(tweets)
+write.csv(tweetsDF,"tweets.csv")
+# lang='en' Means English language only
